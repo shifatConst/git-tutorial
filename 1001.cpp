eb41e5b98d1aaf17c56700ec244c09d4d2b7ee0e
shifatreza <shifatreza44@gmail.com>
2020-06-11 00:41:49 +0600

    Added a new cpp file with some code

---
+++ 1001.cpp
@@ -0,0 +1,15 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+	ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+
+    int A, B, X;
+    cin >> A >> B;
+	X = A + B;
+	cout << "X = " << X << "\n";
+
+    return 0;
+}
